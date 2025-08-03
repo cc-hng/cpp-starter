0f92ee188353ecfdcd3787b300da8e0462712152
cc <cc@l2x.top>
2025-08-03 21:44:45 +0800

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 
 #include "stdafx.h"
-#include <fmt/core.h>
+#include <stdio.h>
+#include <strings.hpp>
 
 int main() {
-    auto _defer = gsl::finally([] { fmt::print("exit.\n"); });
-    fmt::print("hello,world!\n");
-    g_asp.set_timeout(1000, [] { fmt::print("timeout 1s\n"); });
-    g_asp.run(1, false);
+    printf("hello,%s\n", ss::str_tolower("WORLD").c_str());
     return 0;
 }
